033e3b8a7df8101f2a8fa2478937df09d7cc71be
Aaron Lord <aaronlord1@gmail.com>
2014-04-28 12:32:08 +0100

    Added xdebug

--- scripts/php.sh
+++ scripts/php.sh
@@ -26,6 +26,23 @@ sudo apt-get install -y php5-gd
 sudo apt-get install -y php5-mcrypt
 sudo apt-get install -y php5-memcached
 sudo apt-get install -y php5-mysql
+sudo apt-get install -y php5-xdebug
+
+# xdebug Config
+cat > $(find /etc/php5 -name xdebug.ini) << EOF
+zend_extension=$(find /usr/lib/php5 -name xdebug.so)
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9000
+xdebug.scream=0
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+
+; var_dump display
+xdebug.var_display_max_depth = 5
+xdebug.var_display_max_children = 256
+xdebug.var_display_max_data = 1024
+EOF
 
 # Turn on error reporting
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
